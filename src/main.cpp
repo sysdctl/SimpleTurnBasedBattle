7a16ecaa063624f881acd3d092b254d53223ae2b
sysdctl <radesorkh@gmail.com>
2026-09-14 00:15:40 +0330

    fix: configure CMake to find Entities.hpp & extra updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 
 #include "Entities.hpp"
 
+#include <iostream>
+
 int main ()
 {
         Player player("hamed", 10);
         Enemy enemy("Zombie", 30);
+
+        std::cout << player.getName() << " HP : " << player.getHealth() <<'\n';
+        std::cout << enemy.getName() << " HP : " << enemy.getHealth() <<'\n';
+
+        player.attack(enemy);
+
+        std::cout <<'\n';
+        std::cout << enemy.getName() << " HP : " << enemy.getHealth() <<'\n';
 }
